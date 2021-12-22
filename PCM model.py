15da43c4abb8668310155294e2444e246d268398
Isaac Blanc <ib3418@ic.ac.uk>
2021-12-22 18:22:27 +0000

    pipe done
    pipe properties all added. Going to try storing them in a dictionary for neatness

--- PCM model.py
+++ PCM model.py
@@ -1,16 +1,24 @@
 # IMPORTING LIBRARIES
-
+import numpy as np
 
 
 # DEFINING CONSTANTS
-#------------------------------------coolant pipe (copper)
-t=0.0008    # Pipe wall thickness       (m)
-Di=0.0044   # Pipe internal diameter    (m)
-L=3.43      # Length of each pipe       (m)
-km=386      # Thermal conductivity      
 
+# operating conditions
+Tmi=20                  # initial temperature       (C)
 
+# coolant pipe (pure copper) properties
+t=0.0008                # wall thickness            (m)
+Di=0.0044               # internal diameter         (m)
+L=3.43                  # length (per pipe)         (m)
+n=8                     # number of pipes           (unitless)
+km=401                  # thermal conductivity      (W/m*K)     Incropera pg899 for pure copper at 300K
+pm=8933	                # density                   (kg/m^3)    Incropera pg899 for pure copper at 300K
+# --------------------------------------------------------------------------------------------------------
+Do=Di+(2*t)             # external diameter         (m)
+A=np.pi*(Di**2)*0.25    # cross sectional area      (m^3)
+Lc=Di/2                 # characteristic length     (m)         characteristic length is the pipe internal radius
+Asi=np.pi*Di*L          # inner surface area        (m^3)
+Aso=np.pi*Do*L          # outer surface area        (m^3)
 
-
-
-
+# coolant (50/50 water/glycol mix) properties
