86413e1848d4a1dcf4141e7c2e0c652246be8bee
Isaac Blanc <ib3418@ic.ac.uk>
2021-12-22 16:31:05 +0000

    starting to enter constants into python model

---
+++ PCM model.py
@@ -0,0 +1,16 @@
+# IMPORTING LIBRARIES
+
+
+
+# DEFINING CONSTANTS
+#------------------------------------coolant pipe (copper)
+t=0.0008    # Pipe wall thickness       (m)
+Di=0.0044   # Pipe internal diameter    (m)
+L=3.43      # Length of each pipe       (m)
+km=386      # Thermal conductivity      
+
+
+
+
+
+
