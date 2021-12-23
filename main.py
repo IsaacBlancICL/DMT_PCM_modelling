1cd5140294cd55738cb5c3369c59f5918c0d4517
Isaac Blanc <ib3418@ic.ac.uk>
2021-12-23 03:52:47 +0000

    multiple trials of pipe numbers now working
    need to make trial functions a bit neater

---
+++ main.py
@@ -0,0 +1,12 @@
+# IMPORTING LIBRARIES
+import matplotlib.pyplot as plt
+import temp_distr as td
+
+# RUNNING TRIALS
+for number in range(8,40,4):
+    td.trial(number)
+    
+# MAKING GRAPH PRETTY
+plt.xlabel('Distance along pipe (m)')
+plt.ylabel('Fluid temperature (deg C)')
+plt.legend()
