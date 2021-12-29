9e4c9fb3fcc071fd285f74d2f32d3db6c5658cef
Isaac Blanc <ib3418@ic.ac.uk>
2021-12-29 19:03:11 +0000

    EngineTempTime adjusted to hopefully now work, and included in main
    Can't actually test it because Scipy is being weird on my laptop. Matt is gonna try

--- engine_temp.py
+++ engine_temp.py
@@ -1,39 +1,42 @@
 # IMPORTING LIBRARIES
-import numpy as np
 import scipy as sc
-import matplotlib.pyplot as plt
 import parameter_classes as pc
 import temp_distr as td
 
-#Instances of all the classes needed to run td solution
+
+# creating instances of each class (except 'systemClass', which is declared in gradient function 'f')
 case   = pc.caseClass()
 pcm    = pc.pcmClass()
 pipes  = pc.pipesClass(case=case, pcm=pcm)
 fluid  = pc.fluidClass()
-system = pc.systemClass(pipes=pipes, fluid=fluid)
-
-#Rate of heat release of engine, specific heat, and mass. Could be put into a class?
-qe = 1
-cpe = 1
-me = 1
-
-def EngineTempTime(qe,cpe,me,pipes,fluid,system,pcm):
-
-    def qf(system,fluid,pipes,pcm):
-        solution = td.solve(pipes, fluid, pcm, system)              #Temperature distribution along PCM
-        x = solution[0]                                             #Relabeled to help relate to the on-paper maths
-        Tpcm = solution[1]
-
-        qfResult = (system.m*fluid.c/pipes.L) * sc.integrate.simpson(Tpcm,x)    #Mean heat transfered from PCM
-        return(qfResult)
-
-    def f(t, system.Ti):
-        return((1/(me*cpe))*(qe + qf(pipes,fluid,system,pcm)))
-
-    TempTime = sc.integrate.solve_ivp(f,[0,600],system.Ti) # arguments are: (gradient function, range of x values, initial value for T, max step in x)
 
 
+# DEFINING FUNCTION
+def EngineTempTime(case,pcm,pipes,fluid, qe,cpe,me):
 
-    return(TempTime)
+    # DEFINING THE ODE GRADIENT FUNCTION (ie: dT/dt where T is engine temperature)
+    def f(t, T_engine):
+        # an instance of systemClass is declared here, with the PCM inlet temperature set to whatever the engine temperature is
+        system = pc.systemClass(inlet_temp=T_engine, pipes=pipes, fluid=fluid)
+        
+        # working out mean heat transfer from PCM to fluid
+        solution = td.solve(pipes=pipes, fluid=fluid, pcm=pcm, system=system)   #Temperature distribution along PCM
+        x = solution[0]                                                         #Relabeled to help relate to the on-paper maths
+        Tpcm = solution[1]                                                      #Also relabeled
+        qf = (system.m*fluid.c/pipes.L) * sc.integrate.simpson(Tpcm,x)          #Mean heat transfered from PCM
+        
+        # returning gradient
+        return((1/(me*cpe))*(qe + qf))
 
-print(EngineTempTime(qe,cpe,me,pipes,fluid,system,pcm))
+    # SOLVING THE ODE
+    TempTime = sc.integrate.solve_ivp(f, [0,600], [10], max_step=1) # arguments are: (gradient function, range of x values, initial value for T, max step in t)
+
+    # RETURNING [t,T_engine]
+    return [TempTime.t, TempTime.y[0]]
+
+
+
+
+
+
+EngineTempTime(case,pcm,pipes,fluid, qe=1,cpe=1,me=1)
