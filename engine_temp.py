db0f11a502472879f8cca661ad2c5aad3e457ba1
Isaac Blanc <ib3418@ic.ac.uk>
2021-12-29 18:18:37 +0000

    Minor edits to Matt's code to make it fit style

--- engine_temp.py
+++ engine_temp.py
@@ -1,3 +1,4 @@
+# IMPORTING LIBRARIES
 import numpy as np
 import scipy as sc
 import matplotlib.pyplot as plt
@@ -5,12 +6,14 @@ import parameter_classes as pc
 import temp_distr as td
 
 #Instances of all the classes needed to run td solution
-pipes = pc.pipesClass()
-fluid = pc.fluidClass()
-pcm = pc.pcmClass(pipe_external_diam=pipes.Do)
-system = pc.systemClass(pipes, fluid)
+case   = pc.caseClass()
+pcm    = pc.pcmClass()
+pipes  = pc.pipesClass(case=case, pcm=pcm)
+fluid  = pc.fluidClass()
+system = pc.systemClass(pipes=pipes, fluid=fluid)
 
-qe = 1              #Rate of heat release of engine, specific heat, and mass. Could be put into a class?
+#Rate of heat release of engine, specific heat, and mass. Could be put into a class?
+qe = 1
 cpe = 1
 me = 1
 
@@ -21,14 +24,13 @@ def EngineTempTime(qe,cpe,me,pipes,fluid,system,pcm):
         x = solution[0]                                             #Relabeled to help relate to the on-paper maths
         Tpcm = solution[1]
 
-        qf = (system.m*fluid.c/x[-1]) * sc.integrate.simpson(Tpcm,x)    #Mean heat transfered from PCM
-        return(qf)
+        qfResult = (system.m*fluid.c/x[-1]) * sc.integrate.simpson(Tpcm,x)    #Mean heat transfered from PCM
+        return(qfResult)
 
     def f(t, system.Ti):
-
         return((1/(me*cpe))*(qe + qf(pipes,fluid,system,pcm)))
 
-    TempTime = sc.integrate.solve_ivp(f,[0,600],system.Ti)
+    TempTime = sc.integrate.solve_ivp(f,[0,600],system.Ti) # arguments are: (gradient function, range of x values, initial value for T, max step in x)
 
 
 
