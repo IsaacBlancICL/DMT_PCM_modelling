a58044d928d22fbeb295e3a6ecfa8b22feb84d07
Isaac Blanc <ib3418@ic.ac.uk>
2022-01-24 13:02:00 +0000

    Results for Case 1 and Case 3, but values hella low so not using in presentation

--- main.py
+++ main.py
@@ -4,14 +4,24 @@ import parameter_classes as pc
 import temp_distr as td
 
 # RUNNING TRIALS
-for var in range(4,30,4):
-    # creating instances of each class
-    pipes  = pc.pipesClass(number=var)
-    fluid  = pc.fluidClass()
-    pcm    = pc.pcmClass(pipe_external_diam=pipes.Do)
-    system = pc.systemClass(pipes, fluid)
-    # simulating and graphing temp distribution
-    td.graph(pipes,fluid,pcm,system,label=f"{var} pipes")
+
+# case 1
+# creating instances of each class
+pipes  = pc.pipesClass(number=18, length=4.16)
+fluid  = pc.fluidClass()
+pcm    = pc.pcmClass(pipe_external_diam=pipes.Do)
+system = pc.systemClass(pipes, fluid)
+# simulating and graphing temp distribution
+td.graph(pipes,fluid,pcm,system,label="Case 1")
+
+# case 3
+# creating instances of each class
+pipes  = pc.pipesClass(number=9, length=2.7)
+fluid  = pc.fluidClass()
+pcm    = pc.pcmClass(pipe_external_diam=pipes.Do)
+system = pc.systemClass(pipes, fluid)
+# simulating and graphing temp distribution
+td.graph(pipes,fluid,pcm,system,label="Case 3")
     
 # MAKING GRAPH PRETTY
 plt.xlabel('Distance along pipe (m)')
